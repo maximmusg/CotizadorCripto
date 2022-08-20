4e608e2b5f9d310d50c66c415882d2d06d0615aa
maximiliano <maxil5gomez@gmail.com>
2022-08-20 00:45:03 -0300

    Agregado spinner y correccion de algunos errores

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ImagenCripto2 from './img/imagen-criptos.png'
 import ImagenCripto3 from './img/pngegg.png'
 import Formulario from './components/Formulario'
 import Resultado from './components/Resultado'
+import Spinner from './components/Spinner'
 
 const Contenedor = styled.div`
   max-width: 900px;
@@ -47,10 +48,13 @@ function App() {
 
   const [monedas, setMonedas] = useState({})
   const [resultado, setResultado] = useState({})
+  const [cargando, setCargando] = useState(false)
 
   useEffect(() => {
     if(Object.keys(monedas).length > 0){
       const cotizarCripto = async () => {
+        setCargando(true)
+        setResultado({})
         const { moneda, criptomoneda} = monedas
         const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${criptomoneda}&tsyms=${moneda}`
         
@@ -59,6 +63,7 @@ function App() {
 
         setResultado(resultado.DISPLAY[criptomoneda][moneda])
 
+        setCargando(false)
       }
       cotizarCripto()
     }
@@ -75,6 +80,7 @@ function App() {
         <Formulario
           setMonedas={setMonedas}
         />
+        {cargando && <Spinner/>}
         {resultado.PRICE && <Resultado resultado={resultado}/>}
       </div>
     </Contenedor>
